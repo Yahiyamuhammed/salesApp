3c2cef4b097a30895c38eb7457bd569e9e76b751
Yahiya Muhammed <yahiyamuhammed@Yahiyas-MacBook-Pro.local>
2025-12-28 17:53:55 +0530

    fix: changed the db schema

--- src/database/db.js
+++ src/database/db.js
@@ -1,17 +1,16 @@
 import * as SQLite from "expo-sqlite";
 
-export const db = SQLite.openDatabase("app.db");
+export const db = SQLite.openDatabase("market.db");
 
 export const initDB = () => {
-  db.transaction((tx) => {
+  db.transaction(tx => {
     tx.executeSql(`
       CREATE TABLE IF NOT EXISTS shops (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
+        name TEXT UNIQUE,
         latitude REAL,
         longitude REAL,
         accuracy REAL,
-        image TEXT,
         commission REAL,
         created_at TEXT
       );
@@ -20,10 +19,7 @@ export const initDB = () => {
     tx.executeSql(`
       CREATE TABLE IF NOT EXISTS products (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        quantity REAL,
-        unit TEXT,
-        price REAL,
+        name TEXT UNIQUE,
         created_at TEXT
       );
     `);
