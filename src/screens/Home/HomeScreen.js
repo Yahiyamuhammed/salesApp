e881178b406aff5c2ea43a3e2009d27acfe2cd37
Yahiya Muhammed <yahiyamuhammed@Yahiyas-MacBook-Pro.local>
2025-12-28 16:06:27 +0530

    feat: created the bottom tab and homescreen

---
+++ src/screens/Home/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import styles from "./homeStyles";
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
